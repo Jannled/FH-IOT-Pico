75b9a82dc1f13053b568a68e49e4b36d7753b43f
Jannled <Jannled@users.noreply.github.com>
2024-11-05 10:48:06 +0100

    Add Waveshare Example

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
  * @version 0.1
  * @date 2024-10-11
  * 
+ * 
+ * 
  * @copyright MIT License
  */
 
@@ -16,6 +18,8 @@
 // Pull down to cut modem power
 #define PIN_MODEM_SLEEP 14
 
+#define NBIOT
+
 void sendATCommand(const char* command)
 {
 	Serial1.println(command);
@@ -25,22 +29,50 @@ void sendATCommand(const char* command)
 	Serial.println(foo);
 }
 
-void setup() 
+uint8_t send_at(const char* command, const char* expectedResult)
 {
-	pinMode(PIN_MODEM_WAKE, OUTPUT);
-	pinMode(PIN_MODEM_SLEEP, OUTPUT);
+	Serial1.println(command);
+	String foo = Serial1.readString();
+	foo.trim();
 
-	Serial.begin(115200); // USB UART
-	Serial1.begin(115200); // SIM7080G-Cat-M module
+	return foo.compareTo(expectedResult) == 0;
+}
 
-	delay(2000);
-	Serial.println("Hello World");
+void waveshare_example()
+{
+	// Request Manufacturer Identification
+	sendATCommand("AT+GMI");
 
-	// Wake up Modem
-	delay(6000);
-	digitalWrite(PIN_MODEM_SLEEP, HIGH);
-	digitalWrite(PIN_MODEM_WAKE, LOW);
+	// Request TA Serial Number Identification (IMEI)
+	sendATCommand("AT+GSN");
 
+	sendATCommand("AT+CPIN?");
+
+	if(!send_at("AT+CPIN?", "READY"))
+		Serial.println("No SIM inserted");
+
+	Serial.print("Connecting ");
+	for(int i=0; i<10; i++)
+	{
+		if(!send_at("AT+CGATT?", "1"))
+		{
+			Serial.print(".");
+			delay(500);
+		}
+		else
+		{
+			Serial.println("Connected");
+			break;
+		}
+	}
+
+	sendATCommand("AT+CSQ");
+	sendATCommand("AT+CPSI?");
+    sendATCommand("AT+COPS?");
+}
+
+void my_stuff()
+{
 	Serial.println("AT");
 	Serial.setTimeout(5000);
 
@@ -56,10 +88,42 @@ void setup()
 	sendATCommand("ATI");
 	
 	#ifdef NBIOT
+
+	// Request Manufacturer Identification
 	sendATCommand("AT+GMI");
+
+	// Request TA Serial Number Identification (IMEI)
 	sendATCommand("AT+GSN");
+
+	// Request Complete TA Capabilities List
+	sendATCommand("AT+GCAP");
+
+	// Enter PIN
 	sendATCommand("AT+CPIN?");
-	sendATCommand("AT+CIFSR");
+
+	// Operator Selection
+	sendATCommand("AT+COPS?");
+
+	// Network Registration
+	sendATCommand("AT+CREG?");
+	// +CREG: <n>,<stat>[,<lac>,<ci>,<netact>]
+	// stat: 
+	//   1 Registered, home network
+	//   2 Not registered, but MT is currently searching a new operator to register to
+	//   3 Registration denied
+	//   4 Unknown
+	//   5 Registered, roaming
+
+	// Attach or detach from GPRS service
+	sendATCommand("AT+CGATT?");
+
+	// GNSS Power Control
+	sendATCommand("AT+CGNSPWR?");
+
+	// 
+	sendATCommand("AT+CGCONTRDP");
+	
+	//CSOC
 
 	for(int i=0; i<10; i++)
 	{
@@ -119,6 +183,30 @@ void setup()
 		}
 		delay(2000);
 	}*/
+}
+
+void setup() 
+{
+	pinMode(PIN_MODEM_WAKE, OUTPUT);
+	pinMode(PIN_MODEM_SLEEP, OUTPUT);
+
+	Serial.begin(115200); // USB UART
+	Serial1.begin(115200); // SIM7080G-Cat-M module
+	Serial.setTimeout(5000);
+
+	// Wake up Modem
+	delay(500);
+	digitalWrite(PIN_MODEM_SLEEP, HIGH);
+	digitalWrite(PIN_MODEM_WAKE, LOW);
+
+	delay(6000);
+	Serial.println("Hello World");
+
+	// Prime the connection
+	Serial1.println("AT");
+	Serial1.readString();
+
+	waveshare_example();
 }
 
 void loop() 
